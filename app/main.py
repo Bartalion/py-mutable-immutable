70b0b76c9753ecbbaaf2a8a19d2d9bd38a3e1f43
Bartalion <2002volk@gmail.com>
2026-08-12 02:36:59 +0300

    app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
+
 lucky_number = 777
 pi = 3.14
 one_is_a_prime_number = False
 name = "Richard"
+
 my_favourite_films = [
     "The Shawshank Redemption",
     "The Lord of the Rings: The Return of the King",
@@ -9,11 +11,18 @@ my_favourite_films = [
     "The Good, the Bad and the Ugly",
     "The Matrix",
 ]
-profile_info = ("michel", "michel@gmail.com", "12345678")
+
+profile_info = {
+    "name": "michel",
+    "email": "michel@gmail.com",
+    "phone": "12345678",
+}
+
 marks = {
     "John": 4,
     "Sergio": 3,
 }
+
 collection_of_coins = {1, 2, 25}
 
 sorted_variables = {"mutable": [], "immutable": []}
@@ -22,9 +31,10 @@ sorted_variables["immutable"].append(lucky_number)
 sorted_variables["immutable"].append(pi)
 sorted_variables["immutable"].append(one_is_a_prime_number)
 sorted_variables["immutable"].append(name)
-sorted_variables["immutable"].append(profile_info)
 
+sorted_variables["mutable"].append(profile_info)
 sorted_variables["mutable"].append(marks)
 sorted_variables["mutable"].append(collection_of_coins)
 sorted_variables["mutable"].append(my_favourite_films)
+
 print(sorted_variables)
